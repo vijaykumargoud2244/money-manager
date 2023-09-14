256e79889e0e5c3d5d2978893967018057f9f18c
vijaykumargoud2244 <vijaykumargoud1210@gmail.com>
2023-09-14 11:40:11 +0000

    error

---
+++ src/components/MoneyDetails/index.js
@@ -0,0 +1,52 @@
+// Write your code here
+import './index.css'
+
+const MoneyDetails = props => {
+  const {balanceAmount, incomeAmount, expensesAmount} = props
+
+  return (
+    <div className="money-details-container">
+      <div className="balance-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
+          alt="balance"
+          className="details-img"
+        />
+        <div>
+          <p className="details-text" data-testid="balanceAmount">
+            Your Balance
+          </p>
+          <p className="details-money">Rs {balanceAmount}</p>
+        </div>
+      </div>
+      <div className="income-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png"
+          alt="income"
+          className="details-img"
+        />
+        <div>
+          <p className="details-text" data-testid="incomeAmount">
+            Your Income
+          </p>
+          <p className="details-money">Rs {incomeAmount}</p>
+        </div>
+      </div>
+      <div className="expenses-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png"
+          alt="expenses"
+          className="details-img"
+        />
+        <div>
+          <p className="details-text" data-testid="expensesAmount">
+            Your Expenses
+          </p>
+          <p className="details-money">Rs {expensesAmount}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MoneyDetails
